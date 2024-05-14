eba61665dc0dd4a024861a3ac9581312d9992d75
Rahmat Santoso <136348263+RahmatSss@users.noreply.github.com>
2024-05-14 15:55:19 +0800

    praktikum

---
+++ routes.php
@@ -0,0 +1,22 @@
+<?php
+
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = "";
+}
+
+switch ($page) {
+    case "":
+    case "dashboard":
+        include "pages/dashboard.php";
+        break;
+    case "bagian":
+        include "pages/bagian.php";
+        break;
+    case "bagiantambah":
+        include "pages/bagiantambah.php";
+        break;
+    default:
+        include "pages/404.php";
+}
